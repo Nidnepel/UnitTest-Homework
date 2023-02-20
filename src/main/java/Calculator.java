fa0840c9163510f9957202d82b98188ca0d0a09d
DmitriyL <dimalependin@gmail.com>
2023-02-20 17:41:38 +0300

    Added Calculator class

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public int sum (int a, int b) {
+        return a + b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int mult (int a, int b) {
+        return a * b;
+    }
+
+    public int mod (int a, int b) {
+        return a % b;
+    }
+}
